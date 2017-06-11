be5b2fd4096807e5690c767973a6c9d632766366
ufo <liujiankang1@foxmail.com>
2017-06-11 12:17:31 +0800

    执行初始model命令前

---
+++ common/models/BaseQuery.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ufo
+ * Date: 17-6-11
+ * Time: 下午12:01
+ */
+
+namespace common\models;
+
+
+use \yii\db\ActiveQuery;
+
+class BaseModel extends ActiveQuery
+{
+
+}
