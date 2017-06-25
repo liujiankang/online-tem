0af620e959520aee9b37686b1bdb42cbb4de067c
liujiankang <liujiankang1@foxmail.com>
2017-06-25 16:02:53 +0800

    全局时间

--- common/components/DateComponent.php
+++ common/components/DateComponent.php
@@ -13,8 +13,8 @@ use yii\base\Component;
 
 class DateComponent extends Component
 {
-    public $timeDiff;
-    public $now;
+    private $timeDiff;//virtralTime - time()
+    public $initTime;
     public $nowDate;
     public $nowTime;
     public function init()
@@ -28,6 +28,26 @@ class DateComponent extends Component
 
     public function setNowTime(){
 
+    }
+
+    public function setTime($date, $time,$microtime)
+    {
+        if(empty($date)){
+            $date=date('Y-m-d');
+        }
+        if(empty($time)){
+            $time=date('H:i:s');
+        }
+        if(empty($microtime)){
+            $microtime=microtime(true) % 1;
+        }
+        //debug_backtrace();
+        Yii::trace([$time,$date,$microtime]);
+    }
+
+    public function getTime()
+    {
+        return $this->timeDiff + time();
     }
 
 }
